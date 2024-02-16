9172956951df97dfef54ac00459fba44c3dac6b4
Verdo Daviarta <benedictusverdoartha@gmail.com>
2024-02-16 19:19:39 +0700

    Commit160224/1

--- src/main/java/com/juaracoding/UjianMingguke3.java
+++ src/main/java/com/juaracoding/UjianMingguke3.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import java.util.ArrayList;
 
 
@@ -16,6 +17,7 @@ public class UjianMingguke3 {
         System.setProperty("webdriver.chrome.driver", path2);
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.get(url);
 
         System.out.println("open web DemoQA ShopTools");
@@ -50,14 +52,41 @@ public class UjianMingguke3 {
             System.out.println("Failed");
         }
         /* Assert validasi 2 */
-        String expectedTxt = "My Account";
-        String heading = driver.findElement(By.className("page-title")).getText();
+        String expectedTxt = "Tools";
+        String heading = driver.findElement(By.xpath("//Div[@class='navbar-logo']")).getText();
         if (expectedTxt.contains(heading)){
             System.out.println("Passed");
         } else {
             System.out.println("Failed");
         }
 
+        /* Add to Chart */
+        WebElement home = driver.findElement(By.xpath("//*[@id=\"noo-site\"]/header/div[2]/div/div/div/div/a"));
+        home.click();
+        System.out.println("Masuk Kehalaman belanja");
+
+        /*Scroll 2000px secara vertikal*/
+        js.executeScript("window.scrollBy(0,2000)");
+
+        Utils.delay(5);
+
+        WebElement productTokyoTalkies = driver.findElement(By.xpath("//a[normalize-space()='Tokyo Talkies']"));
+        productTokyoTalkies.click();
+        System.out.println("Tokyo Talkies terpilih");
+
+        /*Scroll 2000px secara vertikal*/
+        js.executeScript("window.scrollBy(0,1000)");
+
+        WebElement addChart = driver.findElement((By.xpath("//*[@id=\"product-704\"]/div[1]/div[2]/form/div/div[2]/button")));
+        addChart.click();
+
+        String hasBeenAddedCart = "Tokyo Talkies";
+        String Cart = driver.findElement(By.xpath("//*[@id=\"noo-site\"]/div[2]/div/div/div[1]/div/div")).getText();
+        if (expectedTxt.contains(Cart)){
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
 
 
     }
